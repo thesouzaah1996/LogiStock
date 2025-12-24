b03a306040371296409241c3a64aca56fc83dec6
Matheus Fernando <matheus.de.souza.code@gmail.com>
2025-12-24 13:10:18 -0300

    first commit

---
+++ src/test/java/com/logistock/InventoryManagmentSystem/InventoryManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistock.InventoryManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
